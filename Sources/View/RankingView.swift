5b585cbd9241058f3114cb0be4c822cf372564ac
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2022-07-24 09:37:16 +0300

    Add RankingView

---
+++ Sources/View/RankingView.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+struct RankingView: View {
+    @StateObject var store: RankingStore = .init()
+
+    @Binding var rankingType: RankingType
+    @Binding var country: Country
+    @Binding var categoryFilter: CategoryFilter
+
+    @State private var isSettingsPresented = false
+
+    var body: some View {
+        NavigationView {
+            Group {
+                switch store.state {
+                case .loading:
+                    ProgressView()
+                case let .success(items):
+                    List(items) { item in
+                        AppListItemView(item: item)
+                    }
+                case let .failure(error):
+                    Text(error.localizedDescription)
+                }
+            }
+            .navigationTitle(rankingType.name)
+            .navigationBarItems(trailing: RankingSettingsButton(
+                isSettingsPresented: $isSettingsPresented
+            )
+            )
+            .sheet(isPresented: $isSettingsPresented) {
+                RankingSettingsView(
+                    rankingType: $rankingType,
+                    country: $country,
+                    categoryFilter: $categoryFilter
+                )
+            }
+        }
+        .onAppear {
+            refresh()
+        }
+        .onChange(of: rankingType) { _ in
+            refresh()
+        }
+        .onChange(of: country) { _ in
+            refresh()
+        }
+        .onChange(of: categoryFilter) { _ in
+            refresh()
+        }
+    }
+
+    private func refresh() {
+        store.loadRanking(rankingType, country: country, categoryFilter: categoryFilter)
+    }
+}
+
+private struct RankingSettingsButton: View {
+    @Binding var isSettingsPresented: Bool
+
+    var body: some View {
+        Button(action: {
+            isSettingsPresented = true
+        }) {
+            Image(systemName: "slider.horizontal.3")
+        }
+    }
+}
