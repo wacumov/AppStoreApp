a558c96bd64762b3f04b91382c5b287407199413
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2022-07-24 09:45:01 +0300

    Use RankingView as the root view

--- Sources/AppStoreApp.swift
+++ Sources/AppStoreApp.swift
@@ -2,9 +2,17 @@ import SwiftUI
 
 @main
 struct AppStoreApp: App {
+    @AppStorage("rankingType") var rankingType: RankingType = .topFree
+    @AppStorage("country") var country: Country = .US
+    @AppStorage("categoryFilter") var categoryFilter: CategoryFilter = .allApps
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RankingView(
+                rankingType: $rankingType,
+                country: $country,
+                categoryFilter: $categoryFilter
+            )
         }
     }
 }
